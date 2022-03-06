ccb7df7dbbfa951629418530a43057b52a1ab937
Clément <clement.le-boedec@epitech.eu>
2022-03-06 20:30:24 +0100

    outliers corrected

--- analysis.py
+++ analysis.py
@@ -34,6 +34,29 @@ def standard_deviation(mean, sample):
     return (variance ** 0.5)
 
 
+# Find outliers in dataset with Using Z score
+def detect_outlier(data_):
+    outliers = []
+    threshold = 3
+    nb_outliers = 0
+    mean_ = my_mean(data_)
+    std_ = standard_deviation(mean_, data_)
+    
+    for y in data_:
+        z_score= (y - mean_) / std_ 
+        if np.abs(z_score) > threshold:
+            outliers.append(y)
+            nb_outliers += 1
+    
+    if nb_outliers != 0:
+        print("Number of outliers found: ", nb_outliers, "\nHere is the list of outliers :", end = '')
+        for outlier in outliers:
+            print(" ", outlier, end = '')
+
+    else:
+        print("There is no outliers", end = '')
+
+
 # Create histogram
 def histogram(df):
     global PhysiologicParameters
@@ -41,11 +64,13 @@ def histogram(df):
     for col in df.columns[1:]:
         mean = round(my_mean(df[col]), 2)
         std = round(standard_deviation(mean, df[col]), 2)
-        print("\n-----", PhysiologicParameters[col].value, "-----\nMean : ", mean, "\nStandard Deviation : ", std)
+        print("\n\n-----", PhysiologicParameters[col].value, "-----\nMean : ", mean, "\nStandard Deviation : ", std)
+        detect_outlier(df[col])
         plt.title("Distribution of " + PhysiologicParameters[col].value + ':\n' + f'$\mu={mean}$, $\sigma={std}$',  fontsize = 16)
         plt.xlabel(col)
         df[col].plot(kind='hist')
         plt.savefig(col.lower() + ".pdf")
+        print("\n\n" + col.lower() + ".pdf generated")
         plt.clf()
 
 
@@ -56,20 +81,7 @@ def heatmap(heartDiseases):
 
 
 # Find outliers in dataset
-def old_detect_outlier(data_1):
-    threshold = 3
-    mean_1 = np.mean(data_1)
-    std_1 = np.std(data_1)
-
-    for y in data_1:
-        z_score = (y - mean_1) / std_1
-        if np.abs(z_score) > threshold:
-            outliers.append(y)
-    return outliers
-
-
-# Find outliers in dataset
-def detect_outlier(data):
+def old_detect_outlier(data):
     nb_points = data.shape[0]
 
     print("nb_points", nb_points)
@@ -117,5 +129,3 @@ df = pd.read_csv("heart.csv")
 histogram(df)
 
 heatmap(df)
-
-#old_detect_outlier(df)
