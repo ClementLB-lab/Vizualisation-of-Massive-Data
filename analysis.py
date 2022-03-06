ada0ea6c2d11fa51b8963558b74f1fe37ec31a0a
Clément <clement.le-boedec@epitech.eu>
2022-03-06 20:32:02 +0100

    remove former outliers function

--- analysis.py
+++ analysis.py
@@ -80,49 +80,6 @@ def heatmap(heartDiseases):
     plt.show()
 
 
-# Find outliers in dataset
-def old_detect_outlier(data):
-    nb_points = data.shape[0]
-
-    print("nb_points", nb_points)
-
-    # compute center
-    center = np.mean(data, axis=0)
-    print("compute center", center)
-
-    metric = "custom"
-
-    # Toutes les première colonnes
-    std_x = np.std(data[:, 0])
-    # Toutes les secondes colonnes
-    std_y = np.std(data[:, 1])
-    # factor = 5
-    differences = data-center
-    differences[:, 0] /= std_x
-    differences[:, 1] /= std_y
-    abs_differences = np.abs(differences)
-    distances = np.sum(abs_differences, axis=1)
-    sum_distances = np.sum(distances)
-    mean_distance_to_center = sum_distances/nb_points
-
-    nb_outliers = 0
-    for index in range(nb_points):
-        datapoint = data[index]
-        vector_to_center = datapoint-center
-        distance_to_center = abs(vector_to_center[0]/std_x)+abs(vector_to_center[1]/std_y)
-        if distance_to_center > 2.5*mean_distance_to_center:
-            print(datapoint)
-            print(distance_to_center/mean_distance_to_center)
-            print("")
-            plt.plot(datapoint[0], datapoint[1], "o", color="red", markersize=1.5)
-            nb_outliers += 1
-    print(f"{nb_outliers} outliers")
-
-    plt.title(f"outliers in red, {metric} metric")
-    plt.savefig(f"outliers, metric={metric}.pdf")
-    plt.close()
-
-
 # making data frame 
 df = pd.read_csv("heart.csv")
 
